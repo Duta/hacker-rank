584ee8c0d2b13620260c72bb76f3410af5185279
Bertie Wheen <wheen.b@gmail.com>
2015-01-28 20:00:07 +0000

    Setup code style

--- algorithms/warmup/solve_me_first.c
+++ algorithms/warmup/solve_me_first.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 int add(int a, int b) {
-    return a + b;
+  return a + b;
 }
 
 int main(int argc, char **argv) {
-    int a, b;
-    scanf("%d %d", &a, &b);
-    int sum = add(a, b);
-    printf("%d", sum);
-    return 0;
+  int a, b;
+  scanf("%d %d", &a, &b);
+  int sum = add(a, b);
+  printf("%d", sum);
+  return 0;
 }
